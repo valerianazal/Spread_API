36c2cae751d804dc1627f64eddd7013d699e3255
valerianazal <valeria.nazal@uc.cl>
2024-04-11 00:25:41 -0400

    ft: polling and spread post endpoint

--- endpoints/markets/index.js
+++ endpoints/markets/index.js
@@ -1,6 +1,8 @@
 const getMarketSpread = require('../../useCases/marketSpread/get-market-spread');
 const budaAPI = require('../../externalServices/budaAPI/index');
 const getAllMarketsSpread = require('../../useCases/marketSpread/get-all-markets-spread');
+const upsertMarketSpread = require('../../useCases/marketSpread/upsert-market-spread');
+const compareMarketSpread = require('../../useCases/marketSpread/compare-market-spread');
 
 const router = require('express').Router();
 const budaApi = new budaAPI();
@@ -20,6 +22,28 @@ router.get('/spread', async (request, response) => {
     response.status(200).json({
         'All Markets Spread' : spreads
     });
+});
+
+router.post('/:marketId/spread', async (request, response) => {
+    const marketId = request.params.marketId;
+    await upsertMarketSpread(marketId, budaApi);
+
+    response.status(200).json({
+        'Status' : 'File Updated'
+    });
+});
+
+router.get('/:marketId/polling', async (request, response) => {
+    const marketId = request.params.marketId;
+    const areEqual = await compareMarketSpread(marketId, budaApi);
+
+    if (!areEqual) {
+        response.status(400).json({
+            '[ALERT]' : 'Spread does not match'
+        });
+    }
+
+    response.status(200);
 });
 
 module.exports = router;
